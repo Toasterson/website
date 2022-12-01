18625dbf2ae9717df9ad35ecb8d20993bcdcfcc9
Till Wegmueller <toasterson@gmail.com>
2022-12-01 15:23:48 -0300

    Switching blog overview to timeline component

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 
-  export let data: PageServerData;
+	export let data: PageServerData;
 
+	const date_format_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 </script>
 
-<div>
-	{#each data.posts as {title, summary, path}}
-			<a href="/blog/{path}">
-			<div class="card min-w-full my-10">
-			  <div class="card-body">
-			    <h2 class="card-title">{title}</h2>
-			    <p>{summary}</p>
-			    <div class="card-actions justify-end">Read more...</div>
-			  </div>
+<ol class="border-l border-gray-100">
+	{#each data.posts as { title, summary, path, date }}
+		<li>
+			<div class="flex flex-start items-center pt-3">
+				<div class="bg-gray-100 w-2 h-2 rounded-full -ml-1 mr-3" />
+				<p class="text-gray-100 text-sm">
+					{new Date(date).toLocaleDateString('en-US', date_format_options)}
+				</p>
 			</div>
-		</a>
+			<a href="/blog/{path}">
+				<div class="mt-0.5 ml-4 mb-6">
+					<h4 class="text-gray-300 font-semibold text-xl mb-1.5">{title}</h4>
+					<p class="text-gray-300 mb-3">{summary}</p>
+				</div>
+			</a>
+		</li>
 	{/each}
-</div>
+</ol>
